a3232454c5121a16c0d19045083630021465b330
Ajay481 <iamajayyadav481@gmail.com>
2023-03-29 12:27:21 +0530

    Add To Cart and Quantity Added

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,18 +1,19 @@
 import classes from "./CartButton.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../store/ProductSlice";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.shop.quantity);
 
   const showCartHandler = () => {
     dispatch(cartActions.shopCart());
   };
-  
+
   return (
     <button className={classes.button} onClick={showCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
